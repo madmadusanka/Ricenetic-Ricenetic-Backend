8bbad9b25896ff783673b84f5f8eca14a0889e42
Dilan Madusanka <madmadusanka@gmail.com>
2021-11-24 03:08:51 +0530

    Clean project

--- RiceneticAPI/Controllers/WeatherForecastController.cs
+++ RiceneticAPI/Controllers/WeatherForecastController.cs
@@ -48,7 +48,7 @@ namespace RiceneticAPI.Controllers
             {
                 var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                var pathToSave = Directory.GetCurrentDirectory();
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
